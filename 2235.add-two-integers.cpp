b711500c6caaa9fdfa08bd7ef3de5945756e5bfa
BuchaCero <jlbu971107@gmail.com>
2024-12-29 14:55:47 +0800

    2235. Add Two Integers

---
+++ 2235.add-two-integers.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
+
+int main() {
+    Solution sol;
+    int a, b;
+    cin >> a >> b;
+    int result = sol.sum(a, b);
+    cout << result << endl;
+    return 0;
+}
